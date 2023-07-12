5d5cfee32c047deff574933fb260b7f623dd85d8
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2023-07-12 09:40:31 +0800

    gradle kotlin learning code

---
+++ gradle_kotlin_study/src/main/java/com/cmrhyq/gradle/GradleKotlinStudyApplication.java
@@ -0,0 +1,13 @@
+package com.cmrhyq.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleKotlinStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleKotlinStudyApplication.class, args);
+	}
+
+}
