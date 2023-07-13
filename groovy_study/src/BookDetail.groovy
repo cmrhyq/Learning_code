2e869e1e9361626e2b49c7859ce4c9663ea3c4c1
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2023-07-13 10:40:07 +0800

    update groovy learning code

---
+++ groovy_study/src/BookDetail.groovy
@@ -0,0 +1,20 @@
+/**
+ * <p></p>
+ *
+ * @author Alan Huang
+ * @version v0.0.1
+ * @classname BookDetail.groovy
+ * @project groovy_study
+ * @package
+ * @date 2023/7/12 23:32
+ * @email cmrhyq@163.com
+ * @since v0.0.1
+ */
+class BookDetail {
+    def description="一本教你关键时刻如何选择的书"
+    def bookName="对赌"
+
+    def price(price) {
+        "this book is $price dollar"
+    }
+}
