ab75b4206cf68dce078ce4acf58ea59c5ee2f2f6
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-06-23 21:38:48 +0800

    1.new react demo code

---
+++ React/react-demo/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import logo from "./logo.svg"
+
+
+function App() {
+    return (
+        <div>
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    )
+}
+
+export default App;
